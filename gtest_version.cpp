9833c0d6b262f2ad144715f5b8a0dc63c8da8b1d
Andrey <sim124@rambler.ru>
2026-03-10 19:40:08 +0300

    Init

---
+++ gtest_version.cpp
@@ -0,0 +1,10 @@
+#include "gtest/gtest.h"
+
+
+
+// Assuming you have a function or class to test from your src directory (needs appropriate include and linking)
+
+TEST(gtest_version, BasicAssertions) {
+ 
+    EXPECT_TRUE(3 > 0);
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int, char **) {
+	std::cout << "Hello, world!" << std::endl;
+	std::endl;
+	std::endl;
+	return 0;
+}
